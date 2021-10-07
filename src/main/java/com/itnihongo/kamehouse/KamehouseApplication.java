46e0f91eb2ff5ba313249d1231f8fb11a44b0942
xuanphuc97 <64199092+xuanphuc97@users.noreply.github.com>
2021-10-07 20:02:01 +0700

    Initial Project

---
+++ src/main/java/com/itnihongo/kamehouse/KamehouseApplication.java
@@ -0,0 +1,13 @@
+package com.itnihongo.kamehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KamehouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KamehouseApplication.class, args);
+	}
+
+}
