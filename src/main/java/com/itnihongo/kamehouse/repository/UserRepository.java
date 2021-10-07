bf85ab8c4e6271c77c2ea194fd4a0835fbc92533
xuanphuc97 <64199092+xuanphuc97@users.noreply.github.com>
2021-10-07 23:41:56 +0700

    Create UserRepository.java

---
+++ src/main/java/com/itnihongo/kamehouse/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.itnihongo.kamehouse.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import com.itnihongo.kamehouse.model.User;
+
+// public interface UserRepository extends JpaRepository<User, Interger> {
+
+// }
