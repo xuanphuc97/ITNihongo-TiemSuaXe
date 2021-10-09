82662a51176bb3ef7bd88597daa353359307c64c
xuanphuc97 <64199092+xuanphuc97@users.noreply.github.com>
2021-10-09 12:23:42 +0700

    Update UserServiceImpl.java

--- src/main/java/com/itnihongo/kamehouse/service/Impl/UserServiceImpl.java
+++ src/main/java/com/itnihongo/kamehouse/service/Impl/UserServiceImpl.java
@@ -6,27 +6,21 @@ import com.itnihongo.kamehouse.model.User;
 import com.itnihongo.kamehouse.repository.UserRepository;
 import com.itnihongo.kamehouse.service.UserService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
 
+    @Autowired
     private UserRepository userRepository;
-
-    public UserServiceImpl(UserRepository userRepository) {
-        super();
-        this.userRepository = userRepository;
-    }
 
     @Override
     public List<User> getAllUsers() {
         return userRepository.findAll();
+        // User user = new User(null, "linh99", "khanhlinh@gmail.com", "khanhlinh99",
+        // "Nguyen Khanh Linh", "user", true,
+        // false);
+        // return List.of(user);
     }
-
-    @Override
-    public User saveUser(User user) {
-        user = new User(null, "linh99", "khanhlinh@gmail.com", "khanhlinh99", "Nguyen Khanh Linh", "user", true, false);
-        return userRepository.save(user);
-    }
-
 }
