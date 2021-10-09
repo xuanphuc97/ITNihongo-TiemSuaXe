643fe4a2da5aaff41d1b6cea453955e14571f8cd
xuanphuc97 <64199092+xuanphuc97@users.noreply.github.com>
2021-10-09 12:23:33 +0700

    Update KamehouseApplication.java

--- src/main/java/com/itnihongo/kamehouse/KamehouseApplication.java
+++ src/main/java/com/itnihongo/kamehouse/KamehouseApplication.java
@@ -2,8 +2,13 @@ package com.itnihongo.kamehouse;
 
 import java.util.List;
 
+import com.itnihongo.kamehouse.model.User;
+import com.itnihongo.kamehouse.repository.UserRepository;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,10 +19,14 @@ public class KamehouseApplication {
 		SpringApplication.run(KamehouseApplication.class, args);
 
 	}
+	// @Bean
+	// CommandLineRunner commandLineRunner(UserRepository userRepository) {
+	// return args -> {
+	// User linh = new User(null, "linh99", "khanhlinh@gmail.com", "khanhlinh99",
+	// "Nguyen Khanh Linh", "user",
+	// true, false);
+	// userRepository.save(linh);
+	// };
+	// }
 
-	@GetMapping
-	public String listUsers() {
-		// return userService.getAllUsers();
-		return "Hello";
-	}
 }
