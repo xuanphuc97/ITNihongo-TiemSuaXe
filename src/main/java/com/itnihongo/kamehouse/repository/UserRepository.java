d6238c153cd0e3269fc057a6e7248c58c540f68a
xuanphuc97 <64199092+xuanphuc97@users.noreply.github.com>
2021-10-09 12:23:40 +0700

    Update UserRepository.java

--- src/main/java/com/itnihongo/kamehouse/repository/UserRepository.java
+++ src/main/java/com/itnihongo/kamehouse/repository/UserRepository.java
@@ -1,11 +1,8 @@
 package com.itnihongo.kamehouse.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.itnihongo.kamehouse.model.User;
 
-
-@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
