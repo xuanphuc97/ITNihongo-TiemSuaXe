0f257d8e898709b60a588e3e6ea5a08475ae965c
xuanphuc97 <64199092+xuanphuc97@users.noreply.github.com>
2021-10-10 01:48:37 +0700

    Update UserRepository.java

--- src/main/java/com/itnihongo/kamehouse/repository/UserRepository.java
+++ src/main/java/com/itnihongo/kamehouse/repository/UserRepository.java
@@ -1,8 +1,12 @@
 package com.itnihongo.kamehouse.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.itnihongo.kamehouse.model.User;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+	User findByUsername(String username);
+
+	User findByConfirmationToken(String confirmationToken);
 }
