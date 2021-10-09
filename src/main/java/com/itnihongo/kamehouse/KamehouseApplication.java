99fc9e0514153d09c17c712a6bbc8e0f32b3ac63
xuanphuc97 <64199092+xuanphuc97@users.noreply.github.com>
2021-10-10 01:48:29 +0700

    Update KamehouseApplication.java

--- src/main/java/com/itnihongo/kamehouse/KamehouseApplication.java
+++ src/main/java/com/itnihongo/kamehouse/KamehouseApplication.java
@@ -1,32 +1,16 @@
 package com.itnihongo.kamehouse;
 
-import java.util.List;
-
-import com.itnihongo.kamehouse.model.User;
-import com.itnihongo.kamehouse.repository.UserRepository;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 @RestController
 public class KamehouseApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(KamehouseApplication.class, args);
 
 	}
-	// @Bean
-	// CommandLineRunner commandLineRunner(UserRepository userRepository) {
-	// return args -> {
-	// User linh = new User(null, "linh99", "khanhlinh@gmail.com", "khanhlinh99",
-	// "Nguyen Khanh Linh", "user",
-	// true, false);
-	// userRepository.save(linh);
-	// };
-	// }
 
 }
