4a6ba193af6ae0b0c43cf295a0258c1f6e34bba3
xuanphuc97 <64199092+xuanphuc97@users.noreply.github.com>
2021-10-09 12:23:30 +0700

    Update UserController.java

--- src/main/java/com/itnihongo/kamehouse/controller/UserController.java
+++ src/main/java/com/itnihongo/kamehouse/controller/UserController.java
@@ -5,23 +5,22 @@ import java.util.List;
 import com.itnihongo.kamehouse.model.User;
 import com.itnihongo.kamehouse.service.UserService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
+
+    @Autowired
     private UserService userService;
 
-    public UserController(UserService userServcie) {
-        super();
-        this.userService = userService;
-    }
-
     @GetMapping("/users")
-    public String listUsers() {
-        List<User> a = userService.getAllUsers();
-        User b = a.get(0);
-        return b.toString();
+    public List<User> getAllUsers() {
+
+        List<User> listAllUsers = userService.getAllUsers();
+
+        return listAllUsers;
 
     }
 
