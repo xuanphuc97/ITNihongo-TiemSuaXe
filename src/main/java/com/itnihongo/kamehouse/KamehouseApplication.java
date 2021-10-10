279114c1412db00632215b55ee5c8079a56e65fd
xuanphuc97 <64199092+xuanphuc97@users.noreply.github.com>
2021-10-10 11:07:47 +0700

    Update KamehouseApplication.java

--- src/main/java/com/itnihongo/kamehouse/KamehouseApplication.java
+++ src/main/java/com/itnihongo/kamehouse/KamehouseApplication.java
@@ -3,6 +3,7 @@ package com.itnihongo.kamehouse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
@@ -11,6 +12,11 @@ public class KamehouseApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(KamehouseApplication.class, args);
 
+	}
+
+	@GetMapping
+	String home() {
+		return "Kamehouse api";
 	}
 
 }
