ffa1c409fc90789345faf6a42982819d2a40bb3d
xuanphuc97 <64199092+xuanphuc97@users.noreply.github.com>
2021-11-04 15:53:14 +0700

    Update GarageProfile.js

--- app/src/users/garage/GarageProfile.js
+++ app/src/users/garage/GarageProfile.js
@@ -3,17 +3,20 @@ import axios from "axios";
 import LoadingOverlay from "react-loading-overlay";
 
 import { Link, useHistory, useLocation, useParams } from "react-router-dom";
-import { ListGroup, Button, Form } from "react-bootstrap";
+import { ListGroup, Button, Form, Nav } from "react-bootstrap";
 import "./GarageProfile.scss";
 import { useSelector, useDispatch } from "react-redux";
 import garageApis from "./enum/garage-apis";
 import Loader from "react-loader-spinner";
+import ListComment from "../comment/ListComment";
+import Tabs from 'react-bootstrap/Tabs';
+import Tab from 'react-bootstrap/Tab';
 function GarageProfile() {
   const location = useLocation();
   const id = useParams();
   const history = useHistory();
   const auth = useSelector((state) => state.auth);
-  const [services, setService] = useState([1, 2]);
+  // const [services, setService] = useState([1, 2]);
   const [loading, setLoading] = useState(false);
   const userInfo = auth.user;
   const initialState = {
@@ -25,16 +28,14 @@ function GarageProfile() {
     phoneNumber: "",
     openAt: "",
     closeAt: "",
-    // services: [],
   };
-  // const initialStateServices = {
-  //   servicesName: "",
-  //   price: "",
-
-  //   // services: [],
-  // };
+  const initialStateServices = {
+    servicesName: "",
+    price: "",
+  };
   const [garage, setGarage] = useState(initialState);
-  // const [service, setService] = useState(initialState);
+  const [services, setService] = useState([]);
+
   useEffect(() => {
     const getGarage = async () => {
       try {
@@ -51,6 +52,19 @@ function GarageProfile() {
           openAt: resInfo.startAt,
           closeAt: resInfo.endAt,
         });
+        // setLoading(true);
+      } catch (err) {
+        if (err) {
+          console.log(err);
+        }
+      }
+    };
+    const getService = async () => {
+      try {
+        const res = await axios.get(garageApis.getService(id.id));
+        console.log(res.data);
+        var resInfo = res.data;
+        setService([...services, ...res.data]);
         setLoading(true);
       } catch (err) {
         if (err) {
@@ -58,95 +72,97 @@ function GarageProfile() {
         }
       }
     };
-    // if (id) {
     getGarage();
-    // }
-
-    // return () => {
-    //   setGarage(initialState);
-    // };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [id]);
+    getService();
+  }, []);
 
   return (
     <>
-      {!loading ? (
-        <div className="loader">
-          <Loader
-            type="Oval"
-            color="#00BFFF"
-            height={100}
-            width={100}
-          />
-        </div>
-      ) : (
-        <div className="garage main-flex">
-          <div className="garage-profile ">
-            <div className="flex-row">
-              <h3 className="title">Garage Profile</h3>
+      <Tabs defaultActiveKey="profile" id="" className="nav" variant="pills">
 
-              <div className="avatar-preview">
-                <img
-                  src={
-                    "https://i.pinimg.com/originals/9b/3c/74/9b3c749500e3392efe84df990ed862e6.png"
-                  }
-                  className="profile_img"
-                  style={{ style: "background-image" }}
-                  alt="error"
-                />
+        <Tab eventKey="profile" title="Profile">
+          {/* <Nav.Item className="nav">
+          <Nav.Link href={`/garages/${garage.garageId}`} >Profile</Nav.Link> */}
+          {!loading ? (
+            <div className="loader">
+              <Loader type="Oval" color="#00BFFF" height={100} width={100} />
+            </div>
+          ) : (
+            <div className="garage main-flex">
+              <div className="garage-profile ">
+                <div className="flex-row">
+                  <h3 className="title">Garage Profile</h3>
+
+                  <div className="avatar-preview">
+                    <img
+                      src={
+                        "https://i.pinimg.com/originals/9b/3c/74/9b3c749500e3392efe84df990ed862e6.png"
+                      }
+                      className="profile_img"
+                      style={{ style: "background-image" }}
+                      alt="error"
+                    />
+                  </div>
+                </div>
+                <div className="flex-row"></div>
+                <div className="flex-row">
+                  <span className="label">Name:</span>
+                  <span className="content">{garage.name}</span>
+                </div>
+                <div className="flex-row">
+                  <span className="label">Address:</span>
+                  <span className="content">{garage.address}</span>
+                </div>
+                <div className="flex-row">
+                  <span className="label">Location:</span>
+                  <span className="content">{garage.location}</span>
+                </div>
+                <div className="flex-row">
+                  <span className="label">Phone:</span>
+                  <span className="content">{garage.phoneNumber}</span>
+                </div>
+                <div className="flex-row">
+                  <span className="label">Open At:</span>
+                  <span className="content">{garage.openAt}</span>
+                </div>
+                <div className="flex-row">
+                  <span className="label">Close At:</span>
+                  <span className="content">{garage.closeAt}</span>
+                </div>
+                <div className="btn-container">
+                  <Link to={`/garages/${garage.garageId}/edit`}>
+                    <button className="profile__editbtn">Edit</button>
+                  </Link>
+                </div>
+              </div>
+              <div className="garage-service">
+                <h3 className="title">Services</h3>
+                <div className="service-list">
+                  {services.map((service, idx) => {
+                    return (
+                      <div className="flex-row">
+                        <span className="label service-name">
+                          {idx + 1}. {service.serviceName}
+                        </span>
+                        <p className="cost">{service.servicePrice}</p>
+                      </div>
+                    );
+                  })}
+                </div>
+                <Link to={`/garages/${garage.garageId}/service/new`}>
+                  + Add new
+                </Link>
               </div>
             </div>
-            <div className="flex-row"></div>
-            <div className="flex-row">
-              <span className="label">Name:</span>
-              <span className="content">{garage.name}</span>
-            </div>
-            <div className="flex-row">
-              <span className="label">Address:</span>
-              <span className="content">{garage.address}</span>
-            </div>
-            <div className="flex-row">
-              <span className="label">Location:</span>
-              <span className="content">{garage.location}</span>
-            </div>
-            <div className="flex-row">
-              <span className="label">Phone:</span>
-              <span className="content">{garage.phoneNumber}</span>
-            </div>
-            <div className="flex-row">
-              <span className="label">Open At:</span>
-              <span className="content">{garage.openAt}</span>
-            </div>
-            <div className="flex-row">
-              <span className="label">Close At:</span>
-              <span className="content">{garage.closeAt}</span>
-            </div>
-            <div className="btn-container">
-              <Link to={`/garages/${garage.garageId}/edit`}>
-                <button className="profile__editbtn">Edit</button>
-              </Link>
-            </div>
-          </div>
-          <div className="garage-service">
-            <h3 className="title">Services</h3>
-            <div className="service-list">
-              {services.map((garage, idx) => {
-                return (
-                  <div className="flex-row">
-                    <span className="label service-name">
-                      {idx + 1}. {"Service Name"}
-                    </span>
-                    <p className="cost">{"70.000"}</p>
-                  </div>
-                );
-              })}
-            </div>
-            <br />
+          )}
 
-            <Link to={`/user/garage/service/`}>+ Add new</Link>
-          </div>
-        </div>
-      )}
+        </Tab>
+        <Tab eventKey="comments" title="Comments">
+          <ListComment></ListComment>
+
+        </Tab>
+
+      </Tabs>
     </>
   );
 }
